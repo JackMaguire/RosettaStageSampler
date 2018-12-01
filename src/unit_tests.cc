6d18ce693fdfe4196c316783ba7880d027ca230a
Jack Maguire <jack@med.unc.edu>
2018-12-01 08:33:01 -0500

    adding unit_tests.cc

---
+++ src/unit_tests.cc
@@ -0,0 +1,26 @@
+#include <Trajectory.hh>
+#include <global.hh>
+#include <util.hh>
+
+#include <array>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+
+#define PASS true
+#define FAIL false
+
+int main(){
+  if( run_test_1() ){
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+bool run_test_1(){
+  //Tests
+
+
+  return PASS;
+}
