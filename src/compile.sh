c209917229521ba5aeafe8458038612e0aa61322
Jack Maguire <jack@med.unc.edu>
2018-12-11 10:48:38 -0500

    gdwarf-3 for gdb

--- src/compile.sh
+++ src/compile.sh
@@ -5,12 +5,13 @@ if [[ "$#" -gt "0" ]]; then
     case=$1
 fi
 
-extra="-g -O0 -Wall"
+extra="-g -ggdb -Wall -gdwarf-3"
 #extra="-Ofast"
 #extra="-O3"
 
 CXX="g++-5"
 #CXX="clang++"
 
+echo $CXX $case.cc -o $case -I. -std=c++1z $extra
 $CXX $case.cc -o $case -I. -std=c++1z $extra
 
